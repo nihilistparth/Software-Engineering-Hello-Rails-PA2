38f672fa898b5f9e05d9f57a2486cf34000c9b96
nihilistparth <nihilistcpp@gmail.com>
2024-09-20 01:24:31 -0500

    added sort highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,16 @@ module ApplicationHelper
       end
     end
     def toggle_direction(column)
-      params[:sort] == column && params[:direction] == 'asc' ? 'desc' : 'asc'
+        params[:sort] == column && params[:direction] == 'asc' ? 'desc' : 'asc'
+      end
+    
+      def sort_indicator(column)
+        if params[:sort] == column
+          params[:direction] == 'asc' ? '▲' : '▼'
+        else
+          ''
+        end
+      end
     end
   end
   
