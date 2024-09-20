55db25d2127868978d9204d3a3e3a13e75357fdb
nihilistparth <nihilistcpp@gmail.com>
2024-09-20 00:33:11 -0500

    added CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,23 @@ module ApplicationHelper
     def sortable(column, title = nil)
       title ||= column.titleize
       direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-      link_to title, sort: column, direction: direction
+      link_to title, { sort: column, direction: direction }, class: "sortable"
+    end
+  
+    def sort_class(column)
+      if column == params[:sort]
+        params[:direction] == "asc" ? "sort-asc" : "sort-desc"
+      else
+        ""
+      end
+    end
+  
+    def sort_indicator(column)
+      if column == params[:sort]
+        params[:direction] == "asc" ? "▲" : "▼"
+      else
+        ""
+      end
     end
   end
   
