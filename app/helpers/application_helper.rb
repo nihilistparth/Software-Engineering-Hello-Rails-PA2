f3e65e8c66631978f7b237f28a71d4e196696f5b
nihilistparth <nihilistcpp@gmail.com>
2024-09-20 01:26:36 -0500

    added sort highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,37 +1,16 @@
 module ApplicationHelper
-    def sortable(column, title = nil)
-      title ||= column.titleize
-      direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-      link_to title, { sort: column, direction: direction }, class: "sortable"
-    end
-  
-    def sort_class(column)
-      if column == params[:sort]
-        params[:direction] == "asc" ? "sort-asc" : "sort-desc"
-      else
-        ""
-      end
+    def toggle_direction(column)
+      params[:sort] == column && params[:direction] == 'asc' ? 'desc' : 'asc'
     end
   
     def sort_indicator(column)
-      if column == params[:sort]
-        params[:direction] == "asc" ? "▲" : "▼"
+      if params[:sort] == column
+        params[:direction] == 'asc' ? '▲' : '▼'
       else
-        ""
-      end
-    end
-    def toggle_direction(column)
-        params[:sort] == column && params[:direction] == 'asc' ? 'desc' : 'asc'
-      end
-    
-      def sort_indicator(column)
-        if params[:sort] == column
-          params[:direction] == 'asc' ? '▲' : '▼'
-        else
-          ''
-        end
+        ''
       end
     end
   end
+  
   
   
